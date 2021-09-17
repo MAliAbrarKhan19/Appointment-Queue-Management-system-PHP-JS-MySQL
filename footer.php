f664fecd17bbb09ffa0dcd2a87c14b9c47896dbe
M. Ali Abrar Khan <31814733+MAliAbrarKhan19@users.noreply.github.com>
2021-09-17 21:27:30 +0600

    home page design

---
+++ footer.php
@@ -0,0 +1,29 @@
+
+<!-- master row ends  -->
+  </div>
+</div>
+<!-- master row ends  -->
+
+    <!--Footer -->
+       
+    <div class="row bg-dark fixed-bottom"  style="margin-top: 0px;">
+      <div class="col-md-8 offset-md-2 p-2 "><!-- fixed-bottom -->
+        <hr class=" bg-success">
+        <p  class="text-success text-center">
+       &copy; <?php echo date("Y"); ?> Copyright by Appointment Queue Management System   <a class="text-success text-center" href="https://github.com/ananyasaha2468/ONLINE-LOG-BOOK-LOG-MANAGER"> ID-112</a>
+        </p>
+      </div>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+    
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <!-- Bootstrap CSS -->
+
+    <link rel="stylesheet" type="text/css" href="./bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./bootstrap/css/bootstrap-icons.css">
+    <!-- Bootstrap CSS -->
+    <link rel="icons" type="text/css" href="">
+
+    <title>Appointment-Queue-Management-System</title>
+  </head>
+  <body class="container-fluid bg-primary">
+
+    <!-- ===================== Nav bar ====================== -->
+
+    <div class="row bg-dark">
+      <div class="offset-md-1 col-md-10">
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            
+            <a class="navbar-brand text-succes" href="./index.php"><h4 class="text-success">AppQue _</h4><small class="text-muted">Appointment Queue Management System </small></a>
+
+            <div class="align-left collapse navbar-collapse" id="navbarTogglerDemo03">
+              <ul class="navbar-nav mr-2 mt-2 mt-lg-0">
+                <li class="nav-item active">
+                  <a class="nav-link text-success" href="./index.php">| <i class="bi bi-house-fill"></i> |</a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link text-success" href="list.php">| <i class="bi bi-card-list"></i> Queue |</a>
+                </li>
+               <!--  <li class="nav-item">
+                  <a class="nav-link" href="#">Staff Register</a>
+                </li> -->
+                <li class="nav-item disabled" >
+                  <a class="nav-link" href="./index.php">| <i class="bi bi-eye-fill"></i> Admin |</a>
+                </li>
+                
+              </ul>
+           
+            </div>
+          </nav>
+      </div>  
+    </div>  
+
+    <!-- ===================== Nav bar ====================== -->
+<!-- master row starts -->
+<div class="row">
+<!-- master row starts -->
